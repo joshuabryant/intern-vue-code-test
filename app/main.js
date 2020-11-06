56e444557d3ba19606eeabe0f3157f42f8cc0c5c
Joshua Bryant <joshb@journeygroup.com>
2020-11-06 12:57:11 -0500

    Add first few Vue components (header content)

--- app/main.js
+++ app/main.js
@@ -2,7 +2,34 @@
   // Define components.
   Vue.component('resume-base', {
     props: ['resume'],
-    template: '<pre>{{ resume }}</pre>',
+    template: `<div class="resume">
+      <resume-header :resume="resume" />
+      <pre>{{ resume }}</pre>
+    </div>`,
+  });
+
+  Vue.component('resume-header', {
+    props: ['resume'],
+    template: `<div v-if="resume" class="resume__header">
+      <img v-if="resume.photo" :src="resume.photo" alt="resume.name"/>
+      <h1>{{ resume.name }}</h1>
+      <resume-contact-info :resume="resume" />
+    </div>`,
+  });
+
+  Vue.component('resume-contact-info', {
+    props: ['resume'],
+    template: `<div v-if="resume" class="resume__contact-info">
+      <h2 class="sr-only">Contact information</h2>
+      <ul class="resume__contact">
+        <li v-if="resume.email">
+          <a :href="'mailto:' + resume.email">{{ resume.email }}</a>
+        </li>
+        <li v-if="resume.phone">
+          <a :href="'tel:+1' + resume.phone.replace('.', '')">{{ resume.phone }}</a>
+        </li>
+      </ul>
+    </div>`,
   });
 
   // Instantiate the app.
